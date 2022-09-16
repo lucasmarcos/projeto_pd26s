b652937a10036724552db6c0dbe843949c43ff14
Lucas Marcos <marcos94lucas@gmail.com>
2022-09-16 14:58:23 -0300

    botoes

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,28 @@
-import { Button, View } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
 
-export const Home = () => {
-  return (
-    <View>
-    </View>
-  );
+export const Home = ({ navigation }) => {
+	const makeButton = (route) => (
+		<View style={styles.buttonView}>
+			<Button title={route} onPress={_ => navigation.navigate(route)}/>
+		</View>
+	);
+
+	return (
+		<View style={styles.container}>
+			{makeButton("Aluno")}
+			{makeButton("Projeto")}
+			{makeButton("Professor")}
+			{makeButton("Pesquisa")}
+		</View>
+	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 16,
+	},
+	buttonView: {
+		padding: 16,
+	},
+});
