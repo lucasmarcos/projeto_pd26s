ad63e67af4ec9ee1cb06a02c56d398cf46763310
Lucas Marcos <lucasmarcos@live.com>
2022-09-20 19:47:34 -0300

    firebase

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+
 import { Button, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -7,6 +9,15 @@ import { Aluno } from "./src/Aluno";
 import { Professor } from "./src/Professor";
 import { Projeto } from "./src/Projeto";
 import { Pesquisa } from "./src/Pesquisa";
+
+const firebase = initializeApp({
+  apiKey: "AIzaSyAxd7thAZ3lT6qJaWSL0EcFnyamR1m0Giw",
+  authDomain: "pd26s-ad736.firebaseapp.com",
+  projectId: "pd26s-ad736",
+  storageBucket: "pd26s-ad736.appspot.com",
+  messagingSenderId: "667563279286",
+  appId: "1:667563279286:web:b3ad8fb1fd6159fb433717"
+});
 
 const Stack = createNativeStackNavigator();
 
