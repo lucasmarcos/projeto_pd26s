89108b61682ed20be9adca496922fbe480e6a4d0
Lucas Marcos <lucasmarcos@live.com>
2022-10-06 15:55:32 -0300

    fessora

--- src/Professor.tsx
+++ src/Professor.tsx
@@ -1,5 +1,30 @@
-import { View } from "react-native";
+import { Text, View, FlatList, StyleSheet } from "react-native";
+
+import { PROFESSORES } from "./dados";
+
+const CardProfessor = ({ professor }) => (
+  <View style={styles.card}>
+    <Text>{professor.nome}</Text>
+  </View>
+);
 
 export const Professor = () => {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={PROFESSORES}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => <CardProfessor professor={item} />}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  card: {
+    margin: 16,
+  }
+});
--- src/dados.ts
+++ src/dados.ts
@@ -5,3 +5,16 @@ export const ALUNOS = [
   { id: 4, nome: "Aluno #04" },
   { id: 5, nome: "Aluno #05" },
 ];
+
+export const PROFESSORES = [
+  { id: 1, nome: "Professor #01" },
+  { id: 2, nome: "Professor #02" },
+  { id: 3, nome: "Professor #03" },
+  { id: 4, nome: "Professor #04" },
+  { id: 5, nome: "Professor #05" },
+  { id: 6, nome: "Professor #06" },
+  { id: 7, nome: "Professor #07" },
+  { id: 8, nome: "Professor #08" },
+  { id: 9, nome: "Professor #09" },
+  { id: 10, nome: "Professor #10" },
+];
